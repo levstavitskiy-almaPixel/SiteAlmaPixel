5955b42359d688bad6f943a61b5fd63edcd84de6
levstavitskii <levstavitskiy@alma-pixel.com>
2025-09-15 19:39:47 +0500

    Remove top margin from header to start from screen top

--- src/App.tsx
+++ src/App.tsx
@@ -177,8 +177,7 @@ export default function App() {
             backgroundImage: 'url(/Cloud.png)',
             backgroundSize: 'auto 100%',
             backgroundRepeat: 'repeat-x',
-            backgroundPosition: '0% 20%',
-            marginTop: '200px'
+            backgroundPosition: '0% 20%'
           }}
         >
           <Container>
